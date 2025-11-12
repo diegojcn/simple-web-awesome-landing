75ad0cb9edbb20de9370c1502e51bf9f7cec8039
Diego Neves <diegojcn@gmail.com>
2025-11-12 06:50:23 -0300

    refac: css

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles.css'
-// import all Web Awesome styles, including the default theme
-import '@awesome.me/webawesome/dist/styles/webawesome.css';
+import './main.css'
 
-// Optional: import your preferred theme
-import '@awesome.me/webawesome/dist/styles/themes/awesome.css';
-
-// Load Web Awesome styles + all components ONCE to avoid duplicate registry errors
-import '@awesome.me/webawesome/dist/styles/themes/default.css'
-import '@awesome.me/webawesome/dist/styles/native.css'
-import '@awesome.me/webawesome/dist/styles/utilities.css'
 import '@awesome.me/webawesome/dist/webawesome.js'
-
-
 
 createRoot(document.getElementById('root')!).render(<App />)
